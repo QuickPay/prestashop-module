91d470aa1b2413b16d2d0f6fe32076fa13efef97
Gitdyr <kjeld@mail4us.dk>
2016-02-05 23:52:06 +0100

    Stop execution if PrestaShop version is 1.5 or later.

--- payment.php
+++ payment.php
@@ -6,7 +6,7 @@
 *  @copyright 2015 Quickpay
 *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *
-*  $Date: 2015/08/16 15:17:46 $
+*  $Date: 2016/01/30 07:45:06 $
 *  E-mail: helpdesk@quickpay.net
 */
 
@@ -14,6 +14,8 @@ include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../init.php');
 require_once(dirname(__FILE__).'/quickpay.php');
 
+if (_PS_VERSION_ >= '1.5.0.0')
+	die('Bad version');
 
 $quickpay = new Quickpay();
 $quickpay->payment();
