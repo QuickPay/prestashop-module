ca041c734decb511c17df99cbd1e41c14aaa5b7b
Gitdyr <kjeld@mail4us.dk>
2016-02-05 23:53:39 +0100

    Added message for payment where callback fails.

---
+++ controllers/front/fail.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ *  @author    Kjeld Borch Egevang
+ *  @copyright 2015 Quickpay
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *
+ *  $Date: 2016/01/31 17:51:37 $
+ *  E-mail: helpdesk@quickpay.net
+ */
+
+/**
+ * @since 1.5.0
+ */
+class QuickPayFailModuleFrontController extends ModuleFrontController
+{
+	public function initContent()
+	{
+		parent::initContent();
+		Context::getContext()->smarty->assign('status', Tools::getValue('status'));
+		$this->setTemplate('fail.tpl');
+	}
+}
