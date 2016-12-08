961a0173a0304fc61770d2198d07fafe2576cf62
Gitdyr <kjeld@mail4us.dk>
2016-12-08 22:18:54 +0100

    Validator changes. Inhibit the log message: "Frontcontroller::init - Cart cannot be loaded or an order has already been placed using this cart" when order is complete.

--- controllers/front/complete.php
+++ controllers/front/complete.php
@@ -6,7 +6,7 @@
  *  @copyright 2015 Quickpay
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *
- *  $Date: 2016/07/29 21:22:30 $
+ *  $Date: 2016/11/12 20:35:49 $
  *  E-mail: helpdesk@quickpay.net
  */
 
@@ -15,51 +15,60 @@
  */
 class QuickPayCompleteModuleFrontController extends ModuleFrontController
 {
-	public function initContent()
-	{
-		parent::initContent();
+    public function __construct()
+    {
+        parent::__construct();
+        unset($this->context->cookie->id_cart);
+    }
 
-		$id_cart = Tools::getValue('id_cart');
-		$id_module = Tools::getValue('id_module');
-		$key = Tools::getValue('key');
-		if (!$id_module || !$key)
-			Tools::redirect('history.php');
-		for ($i = 0; $i < 10; $i++)
-		{
-			/* Wait for validation */
-			$trans = Db::getInstance()->getRow('SELECT *
-					FROM '._DB_PREFIX_.'quickpay_execution
-					WHERE `id_cart` = '.$id_cart.'
-					ORDER BY `id_cart` ASC');
-			if ($trans && $trans['accepted'])
-				break;
-			sleep(1);
-		}
-		if ($trans && !$trans['accepted'])
-		{
-			$quickpay = new Quickpay();
-			$setup = $quickpay->getSetup();
-			$json = $quickpay->doCurl('payments/'.$trans['trans_id']);
-			$vars = $quickpay->jsonDecode($json);
-			$json = Tools::jsonEncode($vars);
-			if ($vars->accepted == 1)
-			{
-				$checksum = $quickpay->sign($json, $setup->private_key);
-				$quickpay->validate($json, $checksum, _PS_OS_ERROR_);
-			}
-		}
-		$id_order = Order::getOrderByCartId((int)$id_cart);
-		if (!$id_order)
-			Tools::redirect('history.php');
-		$order = new Order((int)$id_order);
-		$context = Context::getContext();
-		$id_customer = $context->cookie->id_customer;
-		if (!Validate::isLoadedObject($order) ||
-				$order->id_customer != $id_customer)
-			Tools::redirect('history.php');
-		Tools::redirect('index.php?controller=order-confirmation&id_cart='.$id_cart.
-				'&id_module='.$id_module.
-				'&id_order='.$id_order.
-				'&key='.$key);
-	}
+    public function initContent()
+    {
+        parent::initContent();
+
+        $id_cart = Tools::getValue('id_cart');
+        $id_module = Tools::getValue('id_module');
+        $key = Tools::getValue('key');
+        if (!$id_module || !$key) {
+            Tools::redirect('history.php');
+        }
+        for ($i = 0; $i < 10; $i++) {
+            /* Wait for validation */
+            $trans = Db::getInstance()->getRow('SELECT *
+                    FROM '._DB_PREFIX_.'quickpay_execution
+                    WHERE `id_cart` = '.$id_cart.'
+                    ORDER BY `id_cart` ASC');
+            if ($trans && $trans['accepted']) {
+                break;
+            }
+            sleep(1);
+        }
+        if ($trans && !$trans['accepted']) {
+            $quickpay = new Quickpay();
+            $setup = $quickpay->getSetup();
+            $json = $quickpay->doCurl('payments/'.$trans['trans_id']);
+            $vars = $quickpay->jsonDecode($json);
+            $json = Tools::jsonEncode($vars);
+            if ($vars->accepted == 1) {
+                $checksum = $quickpay->sign($json, $setup->private_key);
+                $quickpay->validate($json, $checksum, _PS_OS_ERROR_);
+            }
+        }
+        $id_order = Order::getOrderByCartId((int)$id_cart);
+        if (!$id_order) {
+            Tools::redirect('history.php');
+        }
+        $order = new Order((int)$id_order);
+        $context = Context::getContext();
+        $id_customer = $context->cookie->id_customer;
+        if (!Validate::isLoadedObject($order) ||
+                $order->id_customer != $id_customer) {
+            Tools::redirect('history.php');
+        }
+        Tools::redirect(
+            'index.php?controller=order-confirmation&id_cart='.$id_cart.
+            '&id_module='.$id_module.
+            '&id_order='.$id_order.
+            '&key='.$key
+        );
+    }
 }
--- controllers/front/payment.php
+++ controllers/front/payment.php
@@ -6,7 +6,7 @@
  *  @copyright 2015 Quickpay
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *
- *  $Date: 2015/08/16 15:11:48 $
+ *  $Date: 2016/11/12 20:35:49 $
  *  E-mail: helpdesk@quickpay.net
  */
 
@@ -15,9 +15,9 @@
  */
 class QuickPayPaymentModuleFrontController extends ModuleFrontController
 {
-	public function postProcess()
-	{
-		$quickpay = new Quickpay();
-		$quickpay->payment();
-	}
+    public function postProcess()
+    {
+        $quickpay = new Quickpay();
+        $quickpay->payment();
+    }
 }
