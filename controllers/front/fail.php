65fae91d6a042b44514a25c4ab7a12755694dd6d
Gitdyr <kjeld@mail4us.dk>
2019-01-17 06:33:23 +0100

    Renamed Quickpay to QuickPay. Fixed fail message (currency changed, test order) for PrestaShop 1.7.

--- controllers/front/fail.php
+++ controllers/front/fail.php
@@ -3,10 +3,10 @@
  * NOTICE OF LICENSE
  *
  *  @author    Kjeld Borch Egevang
- *  @copyright 2015 Quickpay
+ *  @copyright 2015 QuickPay
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *
- *  $Date: 2016/11/12 20:35:49 $
+ *  $Date: 2019/01/07 06:37:33 $
  *  E-mail: helpdesk@quickpay.net
  */
 
@@ -18,7 +18,16 @@ class QuickPayFailModuleFrontController extends ModuleFrontController
     public function initContent()
     {
         parent::initContent();
-        Context::getContext()->smarty->assign('status', Tools::getValue('status'));
-        $this->setTemplate('fail.tpl');
+        Context::getContext()->smarty->assign(
+            array(
+                'status' => Tools::getValue('status'),
+                'shop_name' => Configuration::get('PS_SHOP_NAME'),
+            )
+        );
+        if (_PS_VERSION_ >= '1.7.0.0') {
+            $this->setTemplate('module:quickpay/views/templates/front/fail17.tpl');
+        } else {
+            $this->setTemplate('fail.tpl');
+        }
     }
 }
