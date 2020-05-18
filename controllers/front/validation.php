1970a34fa8acc063efe70a9e3072ad13aafd765a
Gitdyr <kjeld@mail4us.dk>
2020-05-18 06:42:00 +0200

    Ignore callback events that are not capture events

--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -6,7 +6,7 @@
  *  @copyright 2015 QuickPay
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *
- *  $Date: 2019/01/07 06:37:33 $
+ *  $Date: 2020/04/09 17:49:23 $
  *  E-mail: helpdesk@quickpay.net
  */
 
@@ -24,7 +24,7 @@ class QuickPayValidationModuleFrontController extends ModuleFrontController
         $checksum = $_SERVER['HTTP_QUICKPAY_CHECKSUM_SHA256'];
 
         $quickpay = new QuickPay();
-        $quickpay->validate($json, $checksum);
+        $quickpay->validate($json, $checksum, _PS_OS_PAYMENT_, true);
         exit(0);
     }
 }
