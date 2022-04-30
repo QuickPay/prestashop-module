a971831c409d3ad65df5c165129f831424763d6e
Kjeld Borch Egevang <kjeld@mail4us.dk>
2022-04-30 06:37:09 +0200

    Add Google Pay

--- quickpay.php
+++ quickpay.php
@@ -19,7 +19,7 @@ class QuickPay extends PaymentModule
     {
         $this->name = 'quickpay';
         $this->tab = 'payments_gateways';
-        $this->version = '4.1.7';
+        $this->version = '4.1.8';
         $this->v15 = _PS_VERSION_ >= '1.5.0.0';
         $this->v16 = _PS_VERSION_ >= '1.6.0.0';
         $this->v17 = _PS_VERSION_ >= '1.7.0.0';
@@ -205,6 +205,8 @@ class QuickPay extends PaymentModule
                 $this->l('Sofort'), 0, 'sofort'),
             array('_QUICKPAY_APPLEPAY', 'applepay',
                 $this->l('Apple Pay'), 0, 'apple-pay'),
+            array('_QUICKPAY_GOOGLE_PAY', 'googlepay',
+                $this->l('Google Pay'), 0, 'google-pay'),
             array('_QUICKPAY_BITCOIN', 'bitcoin',
                 $this->l('Bitcoin'), 0, 'bitcoin'),
             array('_QUICKPAY_VIPPS', 'vipps',
